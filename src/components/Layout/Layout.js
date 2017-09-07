d49d10b0fe2d98b61b812d18a91ae7c57b1f0201
maheshn10 <maheshn10@gmail.com>
2017-09-07 15:21:31 +0200

    help

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SideMenuBar from '../SideMenuBar/SideMenuBar';
 import NavBar from '../NavBar/NavBar';
 import BreadCrumbs from '../BreadCrumbs/BreadCrumbs';
+import Help from '../Help/Help';
 import './Layout.scss';
 
 class Layout extends React.Component {
@@ -14,8 +15,15 @@ class Layout extends React.Component {
         <main className="app">
           <div className="app-wrapper">
             <NavBar />
-            <BreadCrumbs />
-            {this.props.children}
+            <div className="app-container">
+              <div className="container">
+                <div className="page-nav">
+                  <BreadCrumbs />
+                  <Help />
+                </div>
+              </div>
+              {this.props.children}
+            </div>
           </div>
         </main>
       </div>
